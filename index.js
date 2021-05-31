3e6bd6bd5da692d9e998ba64d57d367783235559
Marcela Silveira <celassilveira@hotmail.com>
2021-05-30 17:35:51 -0700

    more stuff

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 var express = require('express');
 var app = express();
-var myRouter = require('./myrouter.js');
-app.use('/',myRouter);
-app.listen(300, function ServerListener() {
-	console.log('Sample App with router');
+
+app.use(express.static('public'));
+
+app.use('/process_get', function handleProcessGet(request, response){
+	retVal = {
+		firstName: request.query.firstName,
+		lastName: request.query.lastName
+	};
+	console.log(retVal);
+	response.send(JSON.stringify(retVal));
+});
+
+var server = app.listen(300, function ServerListener() {
+	var host = server.address().address;
+	var port = server.address().port;
+	console.log("Usisng Forms and Express, listening at http://%$:%$",host,port);
 });
